36ebe3ea91fe3edd6c15782465e1a841d32c3df4
Sai <saisandeepvaddi@gmail.com>
2020-02-17 19:57:15 -0500

    add basic hook code - not tested yet

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,61 @@
-import * as React from 'react';
+import React from 'react';
+import { useMountedState } from 'util-hooks';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+interface IOptions {
+  updateIfMounted: boolean;
+  invokeOnRender: boolean;
+}
+
+export function useApiCall(
+  request: any,
+  options: IOptions = {
+    updateIfMounted: true,
+    invokeOnRender: false,
+  }
+) {
+  const isMounted = useMountedState();
+  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  const updateState = React.useCallback(
+    (updater, data) => {
+      if (options.updateIfMounted) {
+        if (isMounted()) {
+          updater(data);
+        }
+      } else {
+        updater(data);
+      }
+    },
+    [isMounted]
+  );
+
+  const invoke = async (...args: any[]) => {
+    try {
+      updateState(setLoading, true);
+      let responseData = await request(...args);
+      if (responseData) {
+        updateState(setData, responseData);
+      }
+    } catch (error) {
+      console.log('error:', error);
+      updateState(setError, error);
+    } finally {
+      updateState(setLoading, false);
+    }
+  };
+
+  React.useEffect(() => {
+    if (options.invokeOnRender) {
+      invoke();
+    }
+  }, []);
+
+  return {
+    loading,
+    data,
+    error,
+    invoke,
+  };
+}
---
+++ src/util-hooks.tsx
@@ -0,0 +1,13 @@
+import { useRef, useCallback, useEffect } from 'react';
+
+export function useMountedState() {
+  const ref = useRef(false);
+  const isMounted = useCallback(() => ref.current, []);
+  useEffect(() => {
+    ref.current = true;
+    return () => {
+      ref.current = false;
+    };
+  }, []);
+  return isMounted;
+}
